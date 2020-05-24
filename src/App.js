a78f78fd57b29b1f34e9179b6ce02cc4badaf4c2
Samuel Garcia <samugv44@gmail.com>
2020-05-24 14:21:52 -0500

    Initial HomePage done ✅

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import firebase from "firebase";
+import HomeImg from "./BooksImg.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component() {
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+      pictures: [],
+    };
+
+    this.handleAuth = this.handleAuth.bind(this);
+    // this.handleLogout = this.handleLogout.bind(this);
+    // this.renderLoginButton = this.renderLoginButton.bind(this);
+    // this.handleUpload = this.handleUpload.bind(this);
+  }
+  handleAuth() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch((error) => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="Home">
+          <div className="leftContent">
+            <h1>Comparte los archivos de tus clases</h1>
+            <p>El conocimiento deber ser libre</p>
+            <div className="buttons">
+              <button onClick={this.handleAuth} id="logIn">
+                Iniciar Sesión
+              </button>
+              <button id="upLoadFile">
+                <input type="file" />
+              </button>
+              <button id="goFiles">Ver archivos</button>
+            </div>
+          </div>
+          <div className="RightContent">
+            <img src={HomeImg} alt="Icon" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
